ee2c3ba3e7969ce07ce88533312e946e2ddaea6c
annguyenbku1999 <78542632+annguyenbku1999@users.noreply.github.com>
2021-04-20 01:33:46 +0700

    add auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+  // private function reformatRequest($request)
+  // {
+  //   return [
+  //     'page' => $this->array_get($request, 'page', null),
+  //     'movienumber' => $this->array_get($request, 'movienumber', null),
+
+  //   ];
+  // }
+  private function createJsonResult($response)
+  {
+    $result = response()->json($response, 200);
+    return $result
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  }
+
+  private function StringToArray($string)
+  {
+    $result = explode(",", $string);
+    return json_decode(json_encode($result), FALSE);
+  }
+  public function logout(Request $request)
+  {
+    $validator = Validator::make(
+      $request->all(),
+      [
+        "email" => 'required|string',
+        "Token" => 'required|string'
+      ]
+    );
+    if ($validator->fails()) {
+      return response()->json(
+        [$validator->errors()],
+        422
+      );
+    }
+    $user =  (array)DB::table('Moderators')
+      ->where('email', $request['email'])
+      ->select('Token')
+      ->first();
+    if ($user['Token'] == null) {
+      return "You were logout.";
+    } else {
+      if ($request['Token'] == $user['Token']) {
+        $user =  (array)DB::table('Moderators')
+          ->where('email', $request['email'])
+          ->update([
+            'Token' => null
+          ]);
+        return "Successful";
+      }
+      else{
+        return "Token is incorrect!";
+      }
+    }
+  }
+  public function login(Request $request)
+  {
+    $validator = Validator::make(
+      $request->all(),
+      [
+        "email" => 'required|string',
+        "password" => 'required|string'
+      ]
+    );
+    if ($validator->fails()) {
+      return response()->json(
+        [$validator->errors()],
+        422
+      );
+    }
+    $user =  (array)DB::table('Moderators')
+      ->where('email', $request['email'])
+      ->select('id', 'password', 'Token')
+      ->first();
+    if ($user['password'] == null) {
+      return "Email does not exist!";
+    }
+    if ($user['Token'] != null) {
+      return "User is login.";
+    } else {
+      if (Hash::check($request['password'], $user['password']) == true) {
+        $token = Str::random(60);
+        $user =  (array)DB::table('Moderators')
+          ->where('email', $request['email'])
+          ->update([
+            'Token' => $token
+          ]);
+        $result = array(
+          'email' => $request['email'],
+          'token' => $token
+        );
+        return $this->createJsonResult($result);
+      } else {
+        return "Password is incorrect.";
+      }
+    }
+  }
+
+  public function register(Request $request)
+  {
+    $toArrayPreferedGenres = explode(",", $request->PreferedGenres);
+    $validator = Validator::make(
+      $request->all(),
+      [
+        'username'     => 'required|string|between:2,100',
+        'name'     => 'required|string|between:2,100',
+        'email'    => 'required|email|unique:users',
+        'password' => 'required|string|min:6',
+        'SocialMedia' => 'sometimes|string|nullable',
+        // 'gender' => 'required|string',
+        'urlAvatar' => 'sometimes|string|nullable',
+        'created_at' => 'required|date',
+        'updated_at' => 'required|date'
+
+      ]
+    );
+    if ($validator->fails()) {
+      return response()->json(
+        [$validator->errors()],
+        422
+      );
+    }
+    // //Check validate Gender
+    // $gender = $request->gender;
+    // if ($gender != 'Male' && $gender != 'Female' && $gender != 'Non-binary') {
+    //   return response()->json(
+    //     "Gender not correct",
+    //     422
+    //   );
+    // }
+    //Check SocialMedia null
+    $createUser = DB::table('Moderators')
+      ->insert([
+        'username' => $request['username'],
+        'name' => $request['name'],
+        'email' => $request['email'],
+        'password' => Hash::make($request['password']),
+        'SocialMedia' =>  $request['SocialMedia'],
+        // 'gender' =>  $request['gender'],
+        'urlAvatar' => $request['urlAvatar'],
+        'created_at' => $request['created_at'],
+        'updated_at' => $request['updated_at']
+      ]);
+    return $this->createJsonResult($createUser);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,18 @@ URL::forceScheme('https');
     | is assigned the "api" middleware group. Enjoy building your API!
     |
 */
-
+Route::group(
+  [
+    'middleware' => 'api',
+    'namespace'  => 'App\Http\Controllers',
+    'prefix'     => 'auth',
+  ],
+  function ($router) {
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('register', 'AuthController@register')->name('register');
+    Route::post('logout', 'AuthController@logout')->name('logout');
+  }
+);
 Route::group(
   [
     'middleware' => 'api',
